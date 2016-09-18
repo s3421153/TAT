b09d9d65312899f68e87d03bbd1cf275e5838d88
Guy Jordan <Guy@NIT-MBP-01.local>
2016-09-18 21:48:22 +0800

    added includes

---
+++ includes/header.php
@@ -0,0 +1,36 @@
+<?php 
+session_start(); 
+include_once('init.php');
+?>
+
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Team Allocation Tool</title>
+  <meta name="description" content="Chat Application">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="apple-touch-icon" href="favicon.png">
+
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/style.css">
+
+  <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+<body class="light-primary-color primary-text-color">
+
+  <nav class="default-primary-color">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <div class="navbar-brand text-primary-color">
+          <span class="glyphicon glyphicon-home" aria-hidden="true"></span>
+			        T.A.T.   
+        </div>
+      </div>
+    </div>
+  </nav>
+
---
+++ includes/login.php
@@ -0,0 +1,66 @@
+<?php 
+
+session_start(); 
+
+require('init.php');
+
+echo 'logging in..';
+
+// Instantiate User
+$theUser = new User();
+
+echo 'User created..';
+
+// set initial login status
+$_SESSION['loggedIn'] = $theUser->isLoggedIn();
+// clear errors
+$_SESSION['loginError'] = '';
+
+// attempt login
+if ( isset($_POST['login-submit'])
+    && isset($_POST['email']) 
+     && !empty($_POST['email']) 
+      && isset($_POST['password']) 
+       && !empty($_POST['password']) )
+{
+  // TODO: clean inputs
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+  // try to log in
+  $theUser->login( $email, $password );
+
+  // determine success of login attempt
+  if ( $theUser->isLoggedIn() ) 
+  {
+    // user is logged in, set session vars
+    $_SESSION['loggedIn'] = $theUser->isLoggedIn();
+    $_SESSION['username'] = $theUser->getUsername();
+  } 
+  else 
+  {
+    // store error
+    $_SESSION['loginError'] = '<p>Invalid credentials. Try again!</p>';
+
+    // return user from whence they came
+    $uri = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : $_SERVER['HTTP_HOST'];
+    header("Location: $uri");
+  }
+
+
+} 
+else 
+{
+    // store error
+    $_SESSION['loginError'] = '<p>There was an error, please enter email and password.</p>';
+
+    // return user from whence they came
+    $uri = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : $_SERVER['HTTP_HOST'];
+    header("Location: $uri");
+}
+
+// Return user to index
+header('Location: ../index.php');
+exit;
+
+?>
