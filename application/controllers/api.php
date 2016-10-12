b59bff032a096727875ba4158fa861e8f69572d3
Guy J <s3421153@student.rmit.edu.au>
2016-10-12 21:39:10 +0800

    Added API Classes

---
+++ application/controllers/api.php
@@ -0,0 +1,14 @@
+<?php
+
+require APPPATH . '/libraries/REST_Controller.php';
+
+class Example extends REST_Controller {
+	
+function __construct()
+    {
+    parent::__construct();
+    }
+
+}
+
+?>
